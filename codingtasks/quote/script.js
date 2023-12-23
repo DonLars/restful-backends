4055a567e768200997b039f8581a7dad3b616c71
Lars Wagner <lars@tubies.de>
2023-12-23 16:22:28 +0100

    feat: add fetch function

---
+++ codingtasks/quote/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let quoteTag = document.querySelector("quote");
+let authorTag = document.querySelector("label");
+
+function getQuote() {
+  fetch("https://dummy-apis.netlify.app/api/quote")
+    .then((response) => response.json())
+    .then((data) => {
+      //quoteTag.textContent = "quote";
+      //authorTag.textContent = "author";
+
+      quoteTag.append(document.createTextNode("„" + data.quote + "“"));
+      authorTag.append(document.createTextNode("- " + data.author + " -"));
+    });
+}
+
+function render() {
+  document.querySelector("button").addEventListener("click", getQuote);
+}
+
+render();
